aee87206135de7dfb388e32fb8d11de9a6f84f20
Akshay Pandit <panditakshay402@gmail.com>
2026-07-03 10:20:53 +0530

    updated cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,55 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    reportDir: "reports/mochawesome",
+    overwrite: false,
+    html: true,
+    json: true
+  },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+    baseUrl: "https://www.saucedemo.com",
+
+    defaultCommandTimeout: 8000,
+
+    video: false,
+
+    screenshotOnRunFailure: true,
+
+    retries: {
+      runMode: 1,
+      openMode: 0
     },
+
+    specPattern: "cypress/e2e/**/*.cy.js",
+
+    env: {
+      username: "standard_user",
+      password: "secret_sauce"
+    },
+
+    setupNodeEvents(on, config) {
+
+      on("task", {
+
+        logToTerminal(message) {
+          console.log(message);
+          return null;
+        }
+
+      });
+
+    }
+
   },
+
+  blockHosts: [
+    "*.saucedemo.com/service-worker.js"
+  ]
+
 });
